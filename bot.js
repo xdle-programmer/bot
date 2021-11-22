c4f1ba70ca564ade01c22a00a5a68165dc83e70e
ARTllline <vedutenkonikita149@gmail.com>
2021-11-22 19:25:39 +0300

    some staf

--- bot.js
+++ bot.js
@@ -1,27 +1,27 @@
 const {Telegraf} = require('telegraf');
 const bot = new Telegraf('2044194503:AAHGV2gHaykbGQIXT2_ThYtcPSyHQR_LMbo'); //сюда помещается токен, который дал botFather
-const asana = require('asana-api');
+// const asana = require('asana-api');
 
-var client = asana.createClient({
-    oauth: {
-        "accessToken" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemF0aW9uIjoxMjAxMzY5ODQ0NTA3NDc5LCJzY29wZSI6ImRlZmF1bHQgaWRlbnRpdHkiLCJzdWIiOjEyMDA5NDg5NzcxMzY5OTQsImlhdCI6MTYzNjg0MDU5MCwiZXhwIjoxNjM2ODQ0MTkwfQ.OmJP1uEmsX_r0fh1tuMPLCgRqmIXhaAcz-zHAJGqCr8",
-        "refreshToken" : "1/1200948977136994:acedad472fbca85c5a36b979f9f86794",
-        "clientId" : "1201363163818181",
-        "clientSecret" : "a77b31ee447c1d11c88d485ec591097d",
-        "redirectUrl" : "urn:ietf:wg:oauth:2.0:oob"
-    }
-});
-
-client.users.list(function (err,users){
-    console.log(users);
-   console.log(err)
-})
+// var client = asana.createClient({
+//     oauth: {
+//         "accessToken" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemF0aW9uIjoxMjAxMzY5ODQ0NTA3NDc5LCJzY29wZSI6ImRlZmF1bHQgaWRlbnRpdHkiLCJzdWIiOjEyMDA5NDg5NzcxMzY5OTQsImlhdCI6MTYzNjg0MDU5MCwiZXhwIjoxNjM2ODQ0MTkwfQ.OmJP1uEmsX_r0fh1tuMPLCgRqmIXhaAcz-zHAJGqCr8",
+//         "refreshToken" : "1/1200948977136994:acedad472fbca85c5a36b979f9f86794",
+//         "clientId" : "1201363163818181",
+//         "clientSecret" : "a77b31ee447c1d11c88d485ec591097d",
+//         "redirectUrl" : "urn:ietf:wg:oauth:2.0:oob"
+//     }
+// });
+//
+// client.users.list(function (err,users){
+//     console.log(users);
+//    console.log(err)
+// })
 
 // let tokens = {};
 // let setTokenCommand = 'token';
 //
-// bot.start((ctx) => ctx.reply('Welcome')); //ответ бота на команду /start
-// bot.help((ctx) => ctx.reply('Send a Asana token')); //ответ бота на команду /help
+bot.start((ctx) => ctx.reply('Welcome')); //ответ бота на команду /start
+bot.help((ctx) => ctx.reply('Ээээээээто ДОКЕР!!!!!!')); //ответ бота на команду /help
 //
 // bot.command(setTokenCommand, ctx => {
 //     let reply = 'Thank you! Token has been saved!';
@@ -48,6 +48,6 @@ client.users.list(function (err,users){
 //     ctx.reply(reply);
 // });
 //
-// bot.launch(); // запуск бота
+ bot.launch(); // запуск бота
 //
-// console.log("bot_start");
+ console.log("bot_start");
